51df9d03b85c59c058c730f14a40cf72e2bec478
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-01 10:53:12 -0600

    realized multiple tags with same name were being created, updated feature test

--- spec/features/tags/user_sees_job_count_by_tag_spec.rb
+++ spec/features/tags/user_sees_job_count_by_tag_spec.rb
@@ -7,17 +7,20 @@ RSpec.feature "user visits job path" do
                      company: company,
                      city: "Denver",
                      salary: 35000.00)
-    job_2 = Job.create(title: "chief of sad path",
+   tag_1 = Tag.create(name: "engineering")
+   tag_2 = Tag.create(name: "sales")
+   tag_3 = Tag.create(name: "ninja")
+   job_2 = Job.create(title: "chief of sad path",
                      description: "sadness",
                      level_of_interest: 24,
                      company: company,
                      city: "Boulder",
                      salary: 35000.00)
-    job_1.tags.create(name: "engineering")
-    job_1.tags.create(name: "sales")
-    job_1.tags.create(name: "ninja")
-    job_2.tags.create(name: "engineering")
-    job_2.tags.create(name: "sales")
+    JobTag.create(job_id: job_1.id, tag_id: tag_1.id)
+    JobTag.create(job_id: job_1.id, tag_id: tag_2.id)
+    JobTag.create(job_id: job_1.id, tag_id: tag_3.id)
+    JobTag.create(job_id: job_2.id, tag_id: tag_1.id)
+    JobTag.create(job_id: job_2.id, tag_id: tag_2.id)
 
     visit company_job_path(company, job_1)
 
