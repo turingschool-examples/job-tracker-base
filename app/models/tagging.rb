5cc0e0acaddd70db0bb0b6d76109eaa6cd6952c3
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-01 10:30:37 -0600

    create migrations for tags and join table taggings

---
+++ app/models/tagging.rb
@@ -0,0 +1,4 @@
+class Tagging < ApplicationRecord
+  belongs_to :jobs
+  belongs_to :tags
+end
--- spec/features/jobs/user_see_all_tags_for_each_job_spec.rb
+++ spec/features/jobs/user_see_all_tags_for_each_job_spec.rb
@@ -4,19 +4,14 @@ RSpec.describe "User visits individual job page" do
   scenario "and sees all tags for that job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-
+    tag1 = job.tags.create!(title: "resume sent")
+    tag2 = job.tags.create!(title: "interview scheduled")
+    tag3 = job.tags.create!(title: "contacted")
     visit company_job_path(company, job)
 
-    fill_in "tag[title]", with: "resume sent"
-    click_on "Add Tag"
-    fill_in "tag[title]", with: "interview scheduled"
-    click_on "Add Tag"
-    fill_in "tag[title]", with: "contacted"
-    click_on "Add Tag"
-
     expect(current_path).to eq company_job_path(company, job)
-    expect(page).to have_content "resume sent"
-    expect(page).to have_content "interview scheduled"
-    expect(page).to have_content "contacted"
+    expect(page).to have_content item1.title
+    expect(page).to have_content item2.title
+    expect(page).to have_content item3.title
   end
 end
