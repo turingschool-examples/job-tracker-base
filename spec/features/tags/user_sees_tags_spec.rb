722290fe83e5345b983a489c7eaf011ddcd682f7
jtruong2 <jtruong2@comcast.net>
2017-08-01 10:47:49 -0600

    test user sees tag count

--- spec/features/tags/user_sees_tags_spec.rb
+++ spec/features/tags/user_sees_tags_spec.rb
@@ -4,10 +4,21 @@ require 'rails_helper'
       tag = Tag.create(title: "Computer")
       company = Company.create(name: "Xcel Energy")
       job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", company_id: company.id)
-      job_tags = JobsTag.create(tag_id: tag.id, job_id: job.id)
 
       visit company_job_path(company, job)
       save_and_open_page
       expect(page).to have_content(tag.title)
+    end
+
+    scenario "sees a count of jobs for each tag" do
+      # As a user,
+      company = Company.create(name: "Xcel Energy")
+      job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", company_id: company.id)
+      job2 = Job.create(title: "Systems Architect", level_of_interest: 74, city: "Miami", company_id: company.id)
+      job.tags.create(title: "Computer")
+      job2.tags.create(title: "Computer")
+      visit company_job_path(company, job)
+
+      expect(page).to have_content("#{tag.title} - 2")
     end
   end
