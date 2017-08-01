eb78498e68347c47c4aefde34f147f6a3f95c7cc
wratterman <will.ratterman@gmail.com>
2017-08-01 10:11:40 -0600

    story 1 test setup, tag/job-tags join table migrations created

---
+++ spec/features/jobs/user_can_see_tags_associated_with_job_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "User sees a specific job" do
+  scenario "a user sees tags for a specific job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job.tags.create.create!(name: "Tech")
+    job.tags.create.create!(name: "Engineering")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("ESPN")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("Tech")
+    expect(page).to have_content("Engineering")
+  end
+end
