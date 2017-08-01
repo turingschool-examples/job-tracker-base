2152fba4378df0ae4ac7ecbccef36a80e73d7fd8
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-01 10:48:48 -0600

    add job count to tags on job show page

--- spec/features/tags/user_can_see_all_tags_for_job_spec.rb
+++ spec/features/tags/user_can_see_all_tags_for_job_spec.rb
@@ -1,42 +1,40 @@
 require 'rails_helper'
 
 RSpec.feature 'User visits a job show page' do
+  let(:company) { Company.create!(name: 'Cherry Creek Mall') }
+  let(:job_attributes) { {title: 'Janitor', description: 'Cleans stuff',
+                          level_of_interest: 4, city: 'Denver', salary: 24000,
+                          company_id: company.id} }
+  let(:job2_attributes) { {title: 'Security Guard', description: 'Guards stuff',
+                          level_of_interest: 3, city: 'Denver', salary: 25000,
+                          company_id: company.id} }
+  let(:job) { Job.create!(job_attributes) }
+  let(:job2) { Job.create!(job2_attributes) }
+  let(:tag1) { Tag.create!(name: 'Last Resort') }
+  let(:tag2) { Tag.create!(name: 'I hope not') }
+  let(:tag3) { Tag.create!(name: 'Only if I have to') }
+  let(:tag4) { Tag.create!(name: 'Dream Job') }
+
   scenario 'and sees all tags associated with that job' do
-    company = Company.create!(name: 'Cherry Creek Mall')
-    job_attributes = {
-      title: 'Janitor',
-      description: 'Cleans stuff',
-      level_of_interest: 4,
-      city: 'Denver',
-      salary: 24000,
-      company_id: company.id
-    }
-    job = Job.create!(job_attributes)
-
-    tag1 = Tag.create!(name: 'Last Resort')
-    tag2 = Tag.create!(name: 'I hope not')
-    tag3 = Tag.create!(name: 'Only if I have to')
-    tag4 = Tag.create!(name: 'Dream Job')
-
     job.tags << [tag1, tag2, tag3]
 
     visit company_job_path(company, job)
-
-    save_and_open_page
 
     expect(page).to have_content tag1.name
     expect(page).to have_content tag2.name
     expect(page).to have_content tag3.name
     expect(page).to_not have_content tag4.name
   end
-end
 
-# t.string   "title"
-# t.string   "description"
-# t.integer  "level_of_interest"
-# t.datetime "created_at",        null: false
-# t.datetime "updated_at",        null: false
-# t.integer  "company_id"
-# t.string   "city"
-# t.integer  "salary"
-# t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  scenario 'and sees the number of jobs for each tag' do
+    job.tags << [tag1, tag2, tag3]
+    tag1.jobs << job2
+
+    visit company_job_path(company, job)
+    save_and_open_page
+
+    expect(page).to have_content "#{tag1.name} - 2"
+    expect(page).to have_content "#{tag2.name} - 1"
+    expect(page).to have_content "#{tag3.name} - 1"
+  end
+end
