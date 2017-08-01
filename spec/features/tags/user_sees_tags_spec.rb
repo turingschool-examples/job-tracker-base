641b5d84dd2ce457eef9f3785f9f48ab7de24faf
jtruong2 <jtruong2@comcast.net>
2017-08-01 10:28:21 -0600

    create associations between jobs and tags

--- spec/features/tags/user_sees_tags_spec.rb
+++ spec/features/tags/user_sees_tags_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
   RSpec.describe "User visits specific job page" do
     scenario "sees tags associated with that job" do
-      # As a user,
-      tag = Tag.create(title: "Software")
+      tag = Tag.create(title: "Computer")
       company = Company.create(name: "Xcel Energy")
       job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", company_id: company.id)
-      # When I visit a specific job page,
+
       visit company_job_path(company, job)
-      # I see the name of each of the tags associated with that job.
+      save_and_open_page
       expect(page).to have_content(tag.title)
     end
   end
