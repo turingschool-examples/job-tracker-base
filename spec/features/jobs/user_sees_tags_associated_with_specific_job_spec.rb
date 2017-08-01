1ad1351b31136a27c9a63b8a0f183a47dae63dc4
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-01 10:11:00 -0600

    initial commit, created test for first user story: user sees tag

---
+++ spec/features/jobs/user_sees_tags_associated_with_specific_job_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "User sees tags" do
+  scenario "when viewing a specific job page" do
+    company  = Company.create!(name: "Big Blue")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag = job.tags.create!(name: "Job Tag")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Big Blue")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("70")
+    expect(page).to have_content("Job Tag")
+  end
+end
