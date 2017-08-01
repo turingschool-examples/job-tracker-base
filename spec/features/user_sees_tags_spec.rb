fe4e927b557843488291edba98f97836ebc56e02
BFunk <bret.e.funk@gmail.com>
2017-08-01 10:13:31 -0600

    added user_sees_tags test

---
+++ spec/features/user_sees_tags_spec.rb
@@ -0,0 +1,19 @@
+  #When I visit a specific job page,
+  #As a user I see the name of each of the tags associated with that job.
+
+require 'rails_helper'
+
+RSpec.describe "User sees tags" do
+  it "a user sees tags on a specific job page" do
+    company = Company.create(name: "Turning")
+    company.job.create(title: "Ag", description: "work with plants", level_of_interest: 98, city: "Denver", salary: 1)
+    company.job.tags.create(name: "hot job")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Turing")
+    expect(page).to have_content("Ag")
+    expect(page).to have_content("hot job")
+  end
+  end
+
