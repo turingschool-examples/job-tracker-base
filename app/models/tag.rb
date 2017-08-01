ff220de9a4cd988ed716d10e570d216b6cbc5f7f
Michael Centrelli <Acentrelli@gmail.com>
2017-08-01 11:57:46 -0600

    Not Pass Test for Average Tag Salary

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,22 @@ class Tag < ApplicationRecord
   has_many :job_tags
   has_many :jobs, through: :job_tags
 
+#  def average_salary
+ #   self.jobs.map do |job|
+  #    job.salary.map do |salary|
+   #     salary.inject{ |sum, el| sum + el }.to_f / job.size
+    #  end
+     #end
+  #
+  #end
+
+  def average_salary
+    self.jobs.each do |job|
+      job.salary.all
+  end
+  end
+
+  def job_count
+    self.jobs.group(:job_id).count
+  end
 end
