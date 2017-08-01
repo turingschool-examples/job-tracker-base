64f9470621af3b88ae6fe6784bb54480e1d53706
jtruong2 <jtruong2@comcast.net>
2017-08-01 11:00:03 -0600

    build many_to_many setup in test

--- spec/features/tags/user_sees_tags_spec.rb
+++ spec/features/tags/user_sees_tags_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
   RSpec.describe "User visits specific job page" do
     scenario "sees tags associated with that job" do
-      tag = Tag.create(title: "Computer")
       company = Company.create(name: "Xcel Energy")
       job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", company_id: company.id)
+      job.tags.create(title: "Computer")
 
       visit company_job_path(company, job)
-      save_and_open_page
-      expect(page).to have_content(tag.title)
+
+      expect(page).to have_content("Computer")
     end
 
     scenario "sees a count of jobs for each tag" do
-      # As a user,
       company = Company.create(name: "Xcel Energy")
+      tag = Tag.create(title: "Computer")
       job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", company_id: company.id)
       job2 = Job.create(title: "Systems Architect", level_of_interest: 74, city: "Miami", company_id: company.id)
-      job.tags.create(title: "Computer")
-      job2.tags.create(title: "Computer")
+      job.jobs_tags.create(tag: tag)
+      job2.jobs_tags.create(tag: tag)
+      byebug
       visit company_job_path(company, job)
 
-      expect(page).to have_content("#{tag.title} - 2")
+      expect(page).to have_content("Computer - 2")
     end
   end
