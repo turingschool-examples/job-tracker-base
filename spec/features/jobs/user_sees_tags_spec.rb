5af184abeac79997547b367ff6e9d7a602f47cd9
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 10:53:23 -0600

    adds number of jobs with tags test

--- spec/features/jobs/user_sees_tags_spec.rb
+++ spec/features/jobs/user_sees_tags_spec.rb
@@ -9,10 +9,6 @@ describe "User visits job page" do
   end
 
   scenario "a user sees all tags for specific job" do
-    # company = Company.create!(name: "ESPN")
-    # job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    # job.tags.create!(name: "Great Location")
-    # job.tags.create!(name: "Great Merntains")
 
     visit company_job_path(@company, @job)
 
@@ -20,19 +16,22 @@ describe "User visits job page" do
     expect(page).to have_content(@job.tags.last.name)
     expect(page).to have_content(@job.title)
   end
-  #
-  # scenario "a user sees all tags for specific job" do
-  #   company = Company.create!(name: "ESPN")
-  #   job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-  #   job.tags.create!(name: "Great Location")
-  #   job.tags.create!(name: "Great Merntains")
-  #
-  #   visit company_job_path(company, job)
-  #
-  #   expect(page).to have_content(job.tags.first.name)
-  #   expect(page).to have_content(job.tags.last.name)
-  #   expect(page).to have_content(job.title)
-  # end
-  # I also see a count of how many jobs have each specific tag listed.
 
+  scenario "a user sees all tags for specific job" do
+    company2 = Company.create!(name: "Different")
+    job2 = company2.jobs.create!(title: "Design", level_of_interest: 40, city: "Seattle")
+    job2.tags.create!(name: "Great Location")
+    job2.tags.create!(name: "Great Merntains")
+
+    company3 = Company.create!(name: "Even Differenter")
+    job3 = company3.jobs.create!(title: "Software", level_of_interest: 54, city: "Washington")
+    job3.tags.create!(name: "Great Location")
+    job3.tags.create!(name: "Great Pizza")
+
+    visit company_job_path(@company, @job)
+
+    expect(page).to have_content("#{job.tags.first.name} - 3")
+    expect(page).to have_content("#{job.tags.first.name} - 2")
+    expect(page).to have_content(job.title)
+  end
 end
