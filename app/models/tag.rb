0a028388e4d9fa1267d23d7369eb5c3dd19501c4
Wil Donovan <wil.donovan@gmail.com>
2017-08-01 11:16:53 -0600

    added the average salary to tag model due to the fact tthat the calculations aren't being done right and im not letting myself take the time to logically think it out....will try to do that now

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,14 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  
+
   has_many :job_tags
   has_many :jobs, through: :job_tags
+
+  def self.average_salary(job)
+    self.joins(:jobs).where(id: job).average(:salary)
+  end
+
+  def self.avg_salaray
+
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#average_salary" do
+    it "shows average salary" do
+      job     = Job.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 1500)
+      job_two = Job.create!(title: "Project Manager", level_of_interest: 80, city: "Denver", salary: 2100)
+      tag_one = job.tags.create(name: "New")
+      tag_two = job.tags.create(name: "Old")
+      job_two = job_two.tags << tag_two
+      expected = Tag.average_salary(job)
+
+      expect(expected).to eq 1800
+    end
+  end
 end
