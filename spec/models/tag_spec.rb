fc5a0c2876e449393dffd9dd0104db59a16b2c36
Ellen Cooper <ellencooper74@gmail.com>
2017-08-01 10:50:55 -0600

    adds before :each to tag model tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -13,35 +13,26 @@ RSpec.describe Tag, type: :model do
   end
 
   describe "instance methods" do
-    it ".average_salary returns average salary of all associated jobs" do
+    before :each do
       company = Company.create!(name: "ESPN")
       job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 30)
       job_2 = company.jobs.create!(title: "Developer 2", level_of_interest: 70, city: "Denver", salary: 40)
       job_3 = company.jobs.create!(title: "Manager", level_of_interest: 70, city: "Denver", salary: 50)
 
-      engineering = Tag.create!(name: "engineering")
-      tech = Tag.create!(name: "tech")
+      @engineering = Tag.create!(name: "engineering")
+      @tech = Tag.create!(name: "tech")
 
-      engineering.jobs << [job_1, job_2]
-      tech.jobs << [job_1, job_2, job_3]
+      @engineering.jobs << [job_1, job_2]
+      @tech.jobs << [job_1, job_2, job_3]
+    end
 
-      expect(engineering.average_salary).to eq(35)
-      expect(tech.average_salary).to eq(40)
+    it ".average_salary returns average salary of all associated jobs" do
+      expect(@engineering.average_salary).to eq(35)
+      expect(@tech.average_salary).to eq(40)
     end
     it ".job_count returns count of all associated jobs" do
-      company = Company.create!(name: "ESPN")
-      job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 30)
-      job_2 = company.jobs.create!(title: "Developer 2", level_of_interest: 70, city: "Denver", salary: 40)
-      job_3 = company.jobs.create!(title: "Manager", level_of_interest: 70, city: "Denver", salary: 50)
-
-      engineering = Tag.create!(name: "engineering")
-      tech = Tag.create!(name: "tech")
-
-      engineering.jobs << [job_1, job_2]
-      tech.jobs << [job_1, job_2, job_3]
-
-      expect(engineering.job_count).to eq(2)
-      expect(tech.job_count).to eq(3)
+      expect(@engineering.job_count).to eq(2)
+      expect(@tech.job_count).to eq(3)
     end
   end
 end
