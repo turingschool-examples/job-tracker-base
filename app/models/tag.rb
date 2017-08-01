9056212e8d6f9898eb7c15cc695d2540ad40287e
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 11:38:10 -0600

    trying to calculate average salary

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,4 +6,10 @@ class Tag < ApplicationRecord
   def count
     JobTag.where(tag: self).count
   end
+
+  def average_salary
+    JobTag.where(tag: self).find_each(job_ids)
+    binding.byebug
+    Tag.joins(:jobs).where().average("salary").floor
+  end
 end
