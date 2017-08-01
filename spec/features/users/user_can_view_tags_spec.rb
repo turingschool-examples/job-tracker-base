6d8d961eab6ecf6f8b2dc2370813a4e869bb6124
JF-Lalonde <lalondej88@gmail.com>
2017-08-01 10:41:06 -0600

    Refactor test to use before each setup

--- spec/features/users/user_can_view_tags_spec.rb
+++ spec/features/users/user_can_view_tags_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "When a user views a specific job page" do
+  before(:each) do
+    @company = Company.create!(name: "Enron")
+    @job = @company.jobs.create!(title: "Lackey", description: "You'd be the fall guy", level_of_interest: 75, city: "Houston")
+    @tag1 = @job.tags.create!(name: "Exciting")
+    @tag2 = @job.tags.create!(name: "Intriguing")
+  end
   scenario "they see the name of each tag associated with that job" do
-    company = Company.create!(name: "Enron")
-    job = company.jobs.create!(title: "Lackey", description: "You'd be the fall guy", level_of_interest: 75, city: "Houston")
-    tag1 = job.tags.create!(name: "Exciting")
-    tag2 = job.tags.create!(name: "Intriguing")
-    job.tags << [tag1, tag2]
+    @job.tags << [@tag1, @tag2]
 
-    visit company_job_path(company, job)
+    visit company_job_path(@company, @job)
     save_and_open_page
-    expect(page).to have_content(tag1.name)
-    expect(page).to have_content(tag2.name)
+    expect(page).to have_content(@tag1.name)
+    expect(page).to have_content(@tag2.name)
+  end
+
+  scenario "they see a job count for each tag" do
 
   end
 end
