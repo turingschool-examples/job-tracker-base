f37cbfa79b3673e411e21adc21861287ff8275e1
wratterman <will.ratterman@gmail.com>
2017-08-01 10:38:40 -0600

    added tag model testing

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new
+        expect(tag).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "Education")
+        expect(tag).to be_valid
+      end
+
+      it "is invalid with a name that exists" do
+        tag = Tag.create(name: "Education")
+        tag1 = Tag.new(name: "Education")
+        expect(tag1).to be_invalid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      tag = Tag.new(name: "Education")
+      expect(tag).to respond_to(:jobs)
+    end
+  end
 end
