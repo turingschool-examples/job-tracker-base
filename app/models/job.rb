fb6062b9c53aa6567c748764014c11d1ff7b23fc
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-01 10:48:32 -0600

    stuck on jobs - tags relationship

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,6 @@ class Job < ActiveRecord::Base
   belongs_to :company
   has_many :taggings
   has_many :tags, through: :taggings
+
+  accepts_nested_attributes_for :taggings
 end
--- spec/features/jobs/user_see_all_tags_for_each_job_spec.rb
+++ spec/features/jobs/user_see_all_tags_for_each_job_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe "User visits individual job page" do
   scenario "and sees all tags for that job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    tag1 = job.tags.create!(title: "resume sent")
-    tag2 = job.tags.create!(title: "interview scheduled")
-    tag3 = job.tags.create!(title: "contacted")
+
+    tag1 = job.tags.create!(title: "Management")
+    tag2 = job.tags.create!(title: "Development")
+    tag3 = job.tags.create!(title: "Remote")
     visit company_job_path(company, job)
 
     expect(current_path).to eq company_job_path(company, job)
