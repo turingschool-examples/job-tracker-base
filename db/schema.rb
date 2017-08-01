4184cbe869e61903d52a4f1a312954b19d6ed5a4
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-01 10:35:40 -0600

    fixed migration issue with tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731144005) do
+ActiveRecord::Schema.define(version: 20170801163420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,14 @@ ActiveRecord::Schema.define(version: 20170731144005) do
     t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_tags_on_job_id", using: :btree
+  end
+
   add_foreign_key "jobs", "companies"
+  add_foreign_key "tags", "jobs"
 end
