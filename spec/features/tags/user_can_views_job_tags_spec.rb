b55c7b7aaced7dc05a7bba20f076fc6e88f6fc7b
Michael Centrelli <Acentrelli@gmail.com>
2017-08-01 10:08:07 -0600

    Test Added for Viewing A Jobs Tags

---
+++ spec/features/tags/user_can_views_job_tags_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "User can view a jobs tags" do
+  scenario "and see those tags" do
+    company = Company.create(name: "Nike")
+    job = Job.create!(title: "Analyst", description: "Awesome job", level_of_interest: 1, company_id: company.id, city: "Los", salary: 500)
+    tag = Tag.create(title: "Set up interview")
+    tag2 = Tag.create(title: "Callback")
+
+    visit jobs_path(job)
+
+    expect(page).to have_content("Set up interview")
+    expect(page).to have_content("Callback")
+
+  end
+end
