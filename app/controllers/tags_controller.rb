a9643ba5ee99ff35e5abfb4cc575797c5e2d9ca7
Michael Centrelli <Acentrelli@gmail.com>
2017-08-01 10:30:12 -0600

    Test Pass For Creating Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,23 @@ class TagsController < ApplicationController
     @tag = Tag.new
   end
 
+  def index
+    @tag = Tag.all
+  end
 
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to tags_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def tag_params
+  params.require(:tag).permit(:title)
+end
 
 end
--- spec/features/tags/user_can_create_a_tag_spec.rb
+++ spec/features/tags/user_can_create_a_tag_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "User can create a tag" do
-  scenario "for a specific job" do
+  scenario "and see the tag created" do
     company = Company.create(name: "Adidas") 
     job = Job.create!(title: "Analyst", description: "Awesome job", level_of_interest: 1, company_id: company.id, city: "Los", salary: 500)
 
-    visit company_job_path(company, job)
+    visit new_tag_path
 
-    click_link "Add Tag"
     fill_in "tag[title]", with: "Set up interview"
     click_on "Create Tag"
 
-    expect(current_path).to eq(company_job_path(companym job))
     expect(page).to have_content("Set up interview")
   end
 end
