84b81663c0ac86824267882fd4de908813a7df50
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 10:49:38 -0600

    adds before each to job page test

--- spec/features/jobs/user_sees_tags_spec.rb
+++ spec/features/jobs/user_sees_tags_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
 describe "User visits job page" do
-  scenario "a user sees all tags for specific job" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    job.tags.create!(name: "Great Location")
-    job.tags.create!(name: "Great Merntains")
-
-    visit company_job_path(company, job)
-
-    expect(page).to have_content(job.tags.first.name)
-    expect(page).to have_content(job.tags.last.name)
-    expect(page).to have_content(job.title)
+  before(:each) do
+    @company = Company.create!(name: "ESPN")
+    @job = @company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    @job.tags.create!(name: "Great Location")
+    @job.tags.create!(name: "Great Merntains")
   end
+
+  scenario "a user sees all tags for specific job" do
+    # company = Company.create!(name: "ESPN")
+    # job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    # job.tags.create!(name: "Great Location")
+    # job.tags.create!(name: "Great Merntains")
+
+    visit company_job_path(@company, @job)
+
+    expect(page).to have_content(@job.tags.first.name)
+    expect(page).to have_content(@job.tags.last.name)
+    expect(page).to have_content(@job.title)
+  end
+  #
+  # scenario "a user sees all tags for specific job" do
+  #   company = Company.create!(name: "ESPN")
+  #   job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+  #   job.tags.create!(name: "Great Location")
+  #   job.tags.create!(name: "Great Merntains")
+  #
+  #   visit company_job_path(company, job)
+  #
+  #   expect(page).to have_content(job.tags.first.name)
+  #   expect(page).to have_content(job.tags.last.name)
+  #   expect(page).to have_content(job.title)
+  # end
+  # I also see a count of how many jobs have each specific tag listed.
+
 end
