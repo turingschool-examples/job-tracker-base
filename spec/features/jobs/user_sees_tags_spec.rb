c69939f0428c655af3ea5d0b16a58c5dcdea7aa1
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 10:46:11 -0600

    adds test for tags on job show page

---
+++ spec/features/jobs/user_sees_tags_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "User visits job page" do
+  scenario "a user sees all tags for specific job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job.tags.create!(name: "Great Location")
+    job.tags.create!(name: "Great Merntains")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(tag1.name)
+    expect(page).to have_content(tag2.name)
+    expect(page).to have_content(job.title)
+  end
+end
