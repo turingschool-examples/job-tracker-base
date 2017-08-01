83ad72c3cd00b9402fa53a6eebbec7c20c5da0fe
erose357 <erose357@gmail.com>
2017-08-01 11:28:08 -0600

    Write 3rd user story

--- spec/features/tags/user_sees_tags_associated_with_job_spec.rb
+++ spec/features/tags/user_sees_tags_associated_with_job_spec.rb
@@ -31,4 +31,24 @@ describe "User visits a job page" do
     expect(page).to have_content(job.city)
     expect(page).to have_content("Might be interesting - 2")
   end
+
+  scenario "also sees average salary for all jobs with that tag" do
+    company = Company.create(name: "Pizza Planet")
+    company2 = Company.create(name: "Electrical Planet")
+    job = Job.create(title: "Cook", level_of_interest: 50, city: "Mars", salary: 35000, company_id: company.id)
+    job2 = Job.create(title: "Dishwasher", level_of_interest: -50, city: "Uranus", salary: 5000, company_id: company.id)
+    job3 = Job.create(title: "Electrician", level_of_interest: 70, city: "Saturn", salary: 45000, company_id: company2.id)
+    job4 = Job.create(title: "Laborer", level_of_interest: 10, city: "Pluto", salary: 15000, company_id: company2.id)
+
+    tag = Tag.create(name: "Not sure")
+    job.tags << tag
+    job2.tags << tag
+
+    visit company_job_path(company.id, job.id)
+
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(job.title)
+    expect(page).to have_content(job.city)
+    expect(page).to have_content("Not sure - 2 (40000), No thanks - 2 (10000)")
+  end
 end
