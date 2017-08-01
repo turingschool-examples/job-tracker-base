d85e4a2f6e9cd82af37f8a6197f70f8cba9f5ba9
Katie Shermer <katieshermer@gmail.com>
2017-08-01 10:21:03 -0600

    Tag model test done

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
+  validates_presence_of :name
+  validates_uniqueness_of :name
   has_many :job_tags
   has_many :jobs, through: :job_tags
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new()
+        expect(tag).to be_invalid
+      end
+
+      it "has a unique name" do
+        tag_1 = Tag.create(name: "Tag 1")
+        tag_2 = Tag.new(name: "Tag 1")
+        expect(tag_2).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "Tag")
+        expect(tag).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      tag = Tag.new(name: "Tag")
+      expect(tag).to respond_to(:jobs)
+    end
+  end
 end
