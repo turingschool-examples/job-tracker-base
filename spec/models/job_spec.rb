6c7f804f482c303599924f5227f1774fdccb4fee
MikelSage <michael.sagapolutele@yahoo.com>
2017-08-01 11:26:25 -0600

    refactor job_spec to use shoulda matchers

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,38 +2,10 @@ require 'rails_helper'
 
 describe Job do
   describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a title" do
-        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
-        expect(job).to be_invalid
-      end
-
-      it "is invalid without a level of interest" do
-        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
-        expect(job).to be_invalid
-      end
-
-      it "is invalid without a city" do
-        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
-        expect(job).to be_invalid
-      end
-    end
-
-    context "valid attributes" do
-      it "is valid with a title and level of interest" do
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver")
-        expect(job).to be_valid
-      end
-    end
+    it {is_expected.to validate_presence_of(:title)}
+    it {is_expected.to validate_presence_of(:level_of_interest)}
+    it {is_expected.to validate_presence_of(:city)}
+    it {is_expected.to have_many(:job_tags)}
+    it {is_expected.to have_many(:tags).through(:job_tags)}
   end
-
-  describe "relationships" do
-    it "belongs to a company" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
-      expect(job).to respond_to(:company)
-    end
-  end
-
-  it {is_expected.to have_many(:job_tags)}
-  it {is_expected.to have_many(:tags).through(:job_tags)}
 end
