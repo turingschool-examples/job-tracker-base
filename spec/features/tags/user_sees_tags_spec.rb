ee117644da67b9460e696a0dbfaeb04e411285e0
jtruong2 <jtruong2@comcast.net>
2017-08-01 11:07:22 -0600

    test sees average salary

--- spec/features/tags/user_sees_tags_spec.rb
+++ spec/features/tags/user_sees_tags_spec.rb
@@ -17,9 +17,22 @@ require 'rails_helper'
       job2 = Job.create(title: "Systems Architect", level_of_interest: 74, city: "Miami", company_id: company.id)
       job.jobs_tags.create(tag: tag)
       job2.jobs_tags.create(tag: tag)
-      byebug
+
       visit company_job_path(company, job)
 
       expect(page).to have_content("Computer - 2")
+    end
+
+    scenario "sees average salary for tags" do
+      # As a user,
+      company = Company.create(name: "Xcel Energy")
+      tag = Tag.create(title: "Computer")
+      job = Job.create(title: 'Software Engineer', level_of_interest: 100, city: "Denver", salary: 90000, company_id: company.id)
+      job2 = Job.create(title: "Systems Architect", level_of_interest: 74, city: "Miami", salary: 120000, company_id: company.id)
+      job.jobs_tags.create(tag: tag)
+      job2.jobs_tags.create(tag: tag)
+      # When I visit a specific job page,
+      visit
+      # I also see an average salary for all jobs within each specific tag listed.
     end
   end
