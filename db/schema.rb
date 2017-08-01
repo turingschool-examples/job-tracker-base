fd95e14e7d896990c6a17b33b07cbd85b28aa095
erose357 <erose357@gmail.com>
2017-08-01 10:31:06 -0600

    Create tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731144005) do
+ActiveRecord::Schema.define(version: 20170801161845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20170731144005) do
     t.string   "city"
     t.integer  "salary"
     t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "jobs", "companies"
--- spec/features/tags/user_sees_tags_associated_with_job_spec.rb
+++ spec/features/tags/user_sees_tags_associated_with_job_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "User visits a job page" do
   scenario "also sees tags associated with that job" do
     company = Company.create(name: "Pizza Planet")
-    job = Job.create(title: "Cook", level_of_interest: 50, city: "Mars", salary: 35000)
-    tag = tag.create(name: "Food")
+    job = Job.create(title: "Cook", level_of_interest: 50, city: "Mars", salary: 35000, company_id: company.id)
+    tag = Tag.create(name: "Food")
 
-    visit company_job_path(company, job)
+    visit company_job_path(company.id, job.id)
 
     expect(page).to have_content("Pizza Planet")
     expect(page).to have_content("Cook")
