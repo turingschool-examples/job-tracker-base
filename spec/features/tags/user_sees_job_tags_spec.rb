559b6b22bd90886d505571528f79ee2aa53130cc
Katie Shermer <katieshermer@gmail.com>
2017-08-01 10:17:59 -0600

    User story 1 complete

--- spec/features/tags/user_sees_job_tags_spec.rb
+++ spec/features/tags/user_sees_job_tags_spec.rb
@@ -7,7 +7,7 @@ describe "User sees a specific job" do
     tag_3 = Tag.create!(name: "High Interest")
     job.tags << [tag_1, tag_2, tag_3]
     # When I visit a specific job page,
-    visit_company_job_path(job)
+    visit company_job_path(company, job)
     # I see the name of each of the tags associated with that job.
     expect(page).to have_content tag_1.name
     expect(page).to have_content tag_2.name
