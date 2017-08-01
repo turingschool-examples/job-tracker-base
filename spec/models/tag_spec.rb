5134c5200a0f5a1caaffaea962b599b1c31c3ebd
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 10:35:37 -0600

    adds tag model tests

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a title" do
+        tag = Tag.new(name: nil)
+        expect(tag).to be_invalid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs through job tags" do
+      tag = Tag.new(name: "Software")
+      job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver")
+      job_tag = JobTag.new(tag: tag, job: job)
+      expect(tag).to respond_to(:jobs)
+    end
+  end
+end
