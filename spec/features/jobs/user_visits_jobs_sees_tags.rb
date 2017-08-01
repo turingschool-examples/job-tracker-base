21d6f88cdfd78704025166ae7d8999aa163d2dde
Daniel Phillips <danieledward.phillips@gmail.com>
2017-08-01 10:15:04 -0600

    adds test structure, first simple test

---
+++ spec/features/jobs/user_visits_jobs_sees_tags.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+RSpec.describe "user visits job page" do
+  scenario "user sees tag name associated with job" do
+
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Tag:")
+
+  end
+  scenario "user sees count of jobs with  specific tag listed" do
+
+
+
+
+  end
+  scenario "user sees average salary for all jobs from spepcific tag listed" do
+
+
+
+
+  end
+end
