4c110f056293a5bcafa5e52c3ca0f30549a3f451
Katie Shermer <katieshermer@gmail.com>
2017-08-01 11:03:47 -0600

    Added average salary model testing for tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -29,4 +29,27 @@ RSpec.describe Tag, type: :model do
       expect(tag).to respond_to(:jobs)
     end
   end
+
+  describe "methods" do
+    it "its jobs have an average salary" do
+      tag = Tag.new(name: "Tag")
+
+      company = Company.create!(name: "ESPN")
+      company_2 = Company.create!(name: "Sports Authority")
+      company_3 = Company.create!(name: "FedEx")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 70000)
+      job_2 = company_2.jobs.create!(title: "Engineer", level_of_interest: 40, city: "Longmont", salary: 75000)
+      job_3 = company_3.jobs.create!(title: "QA", level_of_interest: 75, city: "Loveland", salary: 80000)
+
+      job.tags << tag
+      job_2.tags << tag
+      job_3.tags << tag
+
+      # tag.jobs << job
+      # tag.jobs << job_2
+      # tag.jobs << job_3
+
+      expect(tag.average_salary).to eq 75000
+    end
+  end
 end
