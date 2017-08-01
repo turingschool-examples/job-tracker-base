61d74ca272d819fe85a5436ad89f45ffe9cb2a43
JF-Lalonde <lalondej88@gmail.com>
2017-08-01 10:47:30 -0600

    Add new test scenario

--- spec/features/users/user_can_view_tags_spec.rb
+++ spec/features/users/user_can_view_tags_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature "When a user views a specific job page" do
     @job = @company.jobs.create!(title: "Lackey", description: "You'd be the fall guy", level_of_interest: 75, city: "Houston")
     @tag1 = @job.tags.create!(name: "Exciting")
     @tag2 = @job.tags.create!(name: "Intriguing")
+    @tag3 = Tag.create!(name: "Intriguing")
   end
   scenario "they see the name of each tag associated with that job" do
-    @job.tags << [@tag1, @tag2]
 
     visit company_job_path(@company, @job)
     save_and_open_page
@@ -17,6 +17,13 @@ RSpec.feature "When a user views a specific job page" do
   end
 
   scenario "they see a job count for each tag" do
+    job2 = @company.jobs.create!(title: "CEO", description: "Make all the money", level_of_interest: 100, city: "Houston")
+    company2 = Company.create!(name: "World Peace")
+    job3 = company2.jobs.create!(title: "Attoryney", description: "Chase the bad guys", level_of_interest: 10, city: "New York")
 
+    job2.tags << [@tag1]
+    job3.tags << [@tag3]
+
+    visit company_job_path(@company, @job)
   end
 end
