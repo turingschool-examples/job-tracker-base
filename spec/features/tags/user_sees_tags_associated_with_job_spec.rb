8518a584ea60e7c846ff5ce21d61f1d3fbc35a28
erose357 <erose357@gmail.com>
2017-08-01 11:06:35 -0600

    Add associated tags to job show page

--- spec/features/tags/user_sees_tags_associated_with_job_spec.rb
+++ spec/features/tags/user_sees_tags_associated_with_job_spec.rb
@@ -4,10 +4,10 @@ describe "User visits a job page" do
   scenario "also sees tags associated with that job" do
     company = Company.create(name: "Pizza Planet")
     job = Job.create(title: "Cook", level_of_interest: 50, city: "Mars", salary: 35000, company_id: company.id)
-    tag = Tag.create(name: "Food")
-byebug
-    visit company_job_path(company.id, job.id)
+    tag = job.tags.create(name: "Food")
 
+    visit company_job_path(company.id, job.id)
+save_and_open_page
     expect(page).to have_content("Pizza Planet")
     expect(page).to have_content("Cook")
     expect(page).to have_content("50")
