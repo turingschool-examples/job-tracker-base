6baf77556fbed089397f1aa00fff8f13c29a1c83
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-01 10:33:24 -0600

    add tag model spec; add attribute validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,3 @@
 class Tag < ApplicationRecord
-  validates :name
+  validates_presence_of :name
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new
+        expect(tag).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "Technology")
+        expect(tag).to be_valid
+      end
+    end
+
+    describe "associations" do
+      it "has many jobs" do
+        tag = Tag.new(name: "Technology")
+        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver")
+
+        tag.jobs << job
+
+        expect(tag.jobs).to include(job)
+        expect(tag).to respond_to(:jobs)
+      end
+    end
+  end
 end
