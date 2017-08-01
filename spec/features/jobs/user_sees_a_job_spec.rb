03618aa6d652c6205e2ff8b0fecf0d27d2451f9e
Wil Donovan <wil.donovan@gmail.com>
2017-08-01 10:31:41 -0600

    got the tags test to past just onto count and then onto average salary

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -4,11 +4,16 @@ describe "User sees a specific job" do
   scenario "a user sees a job for a specific company" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag_one = job.tags.create(name: "Software")
+    tag_two = job.tags.create(name: "Leadership")
 
     visit company_job_path(company, job)
 
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
+    expect(page).to have_content tag_one.name
+    expect(page).to have_content tag_two.name
+    expect(page).to have_content Job.average_salary(tag_one.id)
   end
 end
