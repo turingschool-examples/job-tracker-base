49b8db8c9959dc71d459f660638f500970ee1052
erose357 <erose357@gmail.com>
2017-08-01 11:56:16 -0600

    Add validation tests to tag_spec

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
   has_many :job_tags
   has_many :jobs, through: :job_tags
 
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tag do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new
+        expect(tag).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "Interesting")
+        expect(tag).to be_valid
+      end
+    end
+  end
+
 end
