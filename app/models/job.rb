2e6f7790dab155daa8a5e26f12db508dc88d13aa
JoelLindow <joellindow@gmail.com>
2017-08-02 14:53:23 -0600

    ran out of time. Almost had logic pushed down stack.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,4 @@
 class Job < ActiveRecord::Base
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
-
-  def self.city_count(city_name)
-    where(city: "#{city_name}").count
-  end
 end
