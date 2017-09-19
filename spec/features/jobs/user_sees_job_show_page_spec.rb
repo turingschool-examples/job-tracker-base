ad311021a0cd588afe528511b6d484402bb6bf02
JunePaloma <kborkoski@gmail.com>
2017-09-19 09:15:48 -0600

    Add feature test for user seeing all job tags on job show page

---
+++ spec/features/jobs/user_sees_job_show_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "As a user, I visit a specific job page," do
+  it "I see the name of each of the tags associated with that job." do
+    company = Company.create!(name: "ESPN")
+    job_tab = JobTag.create!(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", job_tag: job_tag)
+  
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Software")
+
+  end
+end
