073342a083993645bfca81259ddd66b538ca38f7
JunePaloma <kborkoski@gmail.com>
2017-09-19 10:21:04 -0600

    Add job_tag model test

--- spec/models/job_tag_spec.rb
+++ spec/models/job_tag_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe JobTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "relationships" do
+  it "belongs to a job " do
+    job = Job.new(title: "Developer", level_of_interest: 70, description: "Wahooo")
+    job_tag = JobTag.new(job_id:job.id, tag_id:tag.id)
+
+    expect(job_tag).to respond_to(:job)
+  end
+  it "belongs to a tag" do
+      tag = Tag.new(title: "Software")
+
+      expect(job_tag).to respond_to(:tag)
+  end
 end
