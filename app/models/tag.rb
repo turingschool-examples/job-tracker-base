12b6e6e6a6a4af42c5d1d74195e004b33b8f1828
jayphodges <jayphodges@gmail.com>
2017-09-19 10:01:13 -0600

    Second test passing, many to many relationship fixed by removing plurals on jobs_tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :jobs_tags
   has_many :jobs, through: :jobs_tags
+
+  def average_salary
+    jobs.average(:salary)
+  end
 end
--- spec/features/tags/user_can_view_tags_spec.rb
+++ spec/features/tags/user_can_view_tags_spec.rb
@@ -17,18 +17,36 @@ describe "User sees a specific job" do
     expect(page).to_not have_content("#{job2.title}")
     expect(page).to_not have_content("#{tag2.name}")
   end
+
+  scenario "and can see the count of jobs that have that tag" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Basket Weaver", level_of_interest: 70, city: "Denver")
+    tag1 = job.tags.create(name: "Good Benefits")
+    tag2 = job2.tags.create(name: "Commute")
+    tag3 = job.tags.create(name: "Coffee")
+    job.tags << tag2
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Commute: 2")
+    expect(page).to have_content("Good Benefits: 1")
+    expect(page).to have_content("Coffee: 1")
+  end
+
+  scenario "and can see the count of jobs that have that tag" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 50000)
+    job2 = company.jobs.create!(title: "Basket Weaver", level_of_interest: 70, city: "Denver", salary: 100000)
+
+  end
+
 end
 
 
 
 
-# As a user,
-# When I visit a specific job page,
-# I see the name of each of the tags associated with that job.
 
-# As a user,
-# When I visit a specific job page,
-# I also see a count of how many jobs have each specific tag listed.
 
 # As a user,
 # When I visit a specific job page,
