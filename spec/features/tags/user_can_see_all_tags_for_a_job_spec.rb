876cbfc3097ca7e44e5fe85c2c7b9bd60b819197
amhursh <amhursh@gmail.com>
2017-09-19 10:00:45 -0600

    Add test that avg job salaries by tag show on job page

--- spec/features/tags/user_can_see_all_tags_for_a_job_spec.rb
+++ spec/features/tags/user_can_see_all_tags_for_a_job_spec.rb
@@ -4,6 +4,7 @@ describe "User can see all tags for a job" do
 	context "user can visit job page" do
 		scenario "and can see all tags associated" do
 			company = Company.create(name: "Turing")
+
 			job = Job.create(title: "Teacher",
 											 description: "Teaches things",
 											 level_of_interest: 49,
@@ -23,6 +24,7 @@ describe "User can see all tags for a job" do
 
 		scenario "and can see the number of jobs associated with each tag" do
 			company = Company.create(name: "Turing")
+
 			job1 = Job.create(title: "Teacher",
 											 description: "Teaches things",
 											 level_of_interest: 49,
@@ -41,12 +43,7 @@ describe "User can see all tags for a job" do
 											 company_id: company.id,
 											 city: "Denver",
 											 salary: 1)
-			job4 = Job.create(title: "Accountant",
-											 description: "Counts things",
-											 level_of_interest: 3,
-											 company_id: company.id,
-											 city: "Denver",
-											 salary: 99999)
+
 			tag1 = Tag.create(name: "Cool")
 			tag2 = Tag.create(name: "Classic Sal")
 			tag3 = Tag.create(name: "Code")
@@ -73,9 +70,64 @@ describe "User can see all tags for a job" do
 			end
 
 		end
+
+		scenario "and can see the average salary for each tag's jobs" do
+			company = Company.create(name: "Turing")
+
+			job1 = Job.create(title: "Teacher",
+											 description: "Teaches things",
+											 level_of_interest: 49,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 1000000)
+			job2 = Job.create(title: "Pizza Guy",
+											 description: "Delivers Pizza",
+											 level_of_interest: 5,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 55555)
+			job3 = Job.create(title: "Student",
+											 description: "Learns things",
+											 level_of_interest: 49,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 1)
+
+			tag1 = Tag.create(name: "Cool")
+			tag2 = Tag.create(name: "Classic Sal")
+			tag3 = Tag.create(name: "Code")
+
+			JobsTag.create(job_id: job1.id, tag_id: tag1.id)
+			JobsTag.create(job_id: job1.id, tag_id: tag2.id)
+			JobsTag.create(job_id: job1.id, tag_id: tag3.id)
+			JobsTag.create(job_id: job2.id, tag_id: tag1.id)
+			JobsTag.create(job_id: job2.id, tag_id: tag2.id)
+			JobsTag.create(job_id: job3.id, tag_id: tag1.id)
+
+			visit company_job_path(company, job1)
+
+			within (".tags-table-#{tag1.id}") do
+				expect(page).to have_content("$351852")
+			end
+
+			within (".tags-table-#{tag2.id}") do
+				expect(page).to have_content("$527777")
+			end
+
+			within (".tags-table-#{tag3.id}") do
+				expect(page).to have_content("$1000000")
+			end
+
+		end
 	end
 end
 
 # As a user,
 # When I visit a specific job page,
-# I also see a count of how many jobs have each specific tag listed.
+# I also see an average salary for all jobs within each specific tag listed.
+
+
+
+
+
+
