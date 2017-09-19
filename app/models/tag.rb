1af70046f943e9572c2dcab15cd9fe96a3fe24e1
JunePaloma <kborkoski@gmail.com>
2017-09-19 10:44:38 -0600

    feature test passing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,12 @@ class Tag < ApplicationRecord
   has_many :job_tags
   has_many :jobs, through: :job_tags
 
+  def count
+    self.jobs.count
+  end
+
+  def average_salary
+    self.jobs.average(:salary).to_i
+  end
+
 end
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe "User sees a specific job" do
+  company = Company.create!(name: "ESPN")
+  tag = Tag.create!(title: "Software")
+  job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 70000)
+  job2 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Chicago", salary: 90000)
+  job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
+  job_tag2 = JobTag.create!(job_id:job2.id, tag_id:tag.id)
+
+
   scenario "a user sees a job for a specific company" do
-
-    company = Company.create!(name: "ESPN")
-    tag = Tag.create!(title: "Software")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 70000)
-    job2 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Chicago", salary: 90000)
-    job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
-    job_tag2 = JobTag.create!(job_id:job2.id, tag_id:tag.id)
-
-
     visit company_job_path(company, job)
 
     expect(page).to have_content("ESPN")
@@ -32,5 +31,4 @@ describe "User sees a specific job" do
         visit company_job_path(company, job)
         expect(page).to have_content("Software - 2 (80000)")
     end
-
 end
