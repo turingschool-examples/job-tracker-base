880f8d02850cba8446492fbcbfe70a000010b8bf
Sam Lim <limsammy1@gmail.com>
2017-09-19 09:22:36 -0600

    Removed fk from tag table

--- spec/models/job_tag_spec.rb
+++ spec/models/job_tag_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe JobTag do
-  describe "associations" do
-    context "jobs have many tags" do
+  context "associations" do
+    it "jobs have many tags" do
       company = Company.new(name: 'Dropbox')
       job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver",
         company_id: company.id)
