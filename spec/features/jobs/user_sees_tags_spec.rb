0855f9f5cee1e070129c9d2f93071ecb4770b81a
Aram Anderson <aram.j.anderson@gmail.com>
2017-09-19 09:37:21 -0600

    Story 2 passing

--- spec/features/jobs/user_sees_tags_spec.rb
+++ spec/features/jobs/user_sees_tags_spec.rb
@@ -14,5 +14,20 @@ feature "As a user" do
       expect(page).to have_content(tag1.name)
       expect(page).to have_content(tag2.name)
     end
+    scenario "I also see a count of how many jobs have each specific tag listed" do
+      company1 = Company.create(name: "company1")
+      company2 = Company.create(name: "company2")
+      job1 = company1.jobs.create(title: Faker::Job.title, description: "What a great position!", level_of_interest: 88, city: Faker::Address.city, salary: 80000)
+      job2 = company2.jobs.create(title: Faker::Job.title, description: "What a great position!", level_of_interest: 76, city: Faker::Address.city, salary: 60000)
+      tag1 = Tag.create(name: "industry1")
+      tag2 = Tag.create(name: "industry2")
+      job1.tags << [tag1, tag2]
+      job2.tags << tag1
+
+      visit company_job_path(company1, job1)
+
+      expect(page).to have_content("There are 2 jobs with this tag")
+      expect(page).to have_content("There are 1 jobs with this tag")
+    end
   end
 end
