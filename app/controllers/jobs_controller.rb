8d5276680e0e05278d8bf22e7972bddc5607f126
randy-darsh <rspringer47@gmail.com>
2017-09-19 09:47:40 -0600

    setting up tags jobs relationship

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -22,6 +22,7 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    @tag = Tag.find(params[:id])
   end
 
   def edit
--- spec/features/jobs/user_sees_each_tag_associated_with_a_job_spec.rb
+++ spec/features/jobs/user_sees_each_tag_associated_with_a_job_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 describe 'User visits specific job page' do
   scenario 'user sees each of the tags associated with that job' do
     company = Company.create(id: 12, name: 'we do stuff here')
-    job = Job.create(title: 'stuff doer', description: 'you do stuff', level_of_interest: 8, city: 'city', salary: 10, company_id: 12)
+    job = Job.create(id: 20, title: 'stuff doer', description: 'you do stuff', level_of_interest: 8, city: 'city', salary: 10, company_id: 12)
+    tag = Tag.create(id: 20, title: 'super cool', jobs_id: 20)
 
     visit ("/companies/#{company.id}/jobs/#{job.id}")
+
+    save_and_open_page
 
     expect(page).to have_content("Title: stuff doer")
     expect(page).to have_content("Description: you do stuff")
     expect(page).to have_content("Level of Interest: 8")
     expect(page).to have_content("City: city")
     expect(page).to have_content("Company: we do stuff here")
+    expect(page).to have_content("Tags: super cool")
 
   end
 end
