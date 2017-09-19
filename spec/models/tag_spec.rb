05076af686b8334b110aa543e04b68c367237b28
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-19 09:55:31 -0600

    add model testing on tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can count jobs" do
+  tag = Tag.create(name: "Tech")
+  tag.jobs.create(title: "Teacher", level_of_interest: 70, city: "Denver")
+
+  expect(tag.job_count).to eq(1)
+  end
+  it "can avaerage salaries" do
+  tag = Tag.create(name: "Tech")
+  tag.jobs.create(title: "Teacher", level_of_interest: 70, city: "Denver", salary: 100000)
+  job1 = Job.create(title: "Admin", level_of_interest: 70, city: "Denver", salary: 50000)
+  job1.tags << tag
+
+  expect(tag.avg_salary).to eq(75000)
+  end
 end
+
