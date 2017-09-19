8fd980e4b97e87b0ec2cfd1640909cc71ec273b8
Aram Anderson <aram.j.anderson@gmail.com>
2017-09-19 09:49:20 -0600

    Add model tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tag do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new()
+        expect(tag).to be_invalid
+      end
+
+      it "has a unique name" do
+        Tag.create(name: "industry")
+        tag = Tag.new(name: "industry")
+        expect(tag).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "industry")
+        expect(tag).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      tag = Tag.new(name: "industry")
+      expect(tag).to respond_to(:jobs)
+    end
+  end
 end
