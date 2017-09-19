5e811ada5525e86ab2330a57558905ad44971bd2
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-19 09:24:48 -0600

    add user sees count of jobs per tag

--- spec/features/jobs/user_sees_tags_on_job_spec.rb
+++ spec/features/jobs/user_sees_tags_on_job_spec.rb
@@ -19,4 +19,20 @@ describe "User sees a specific job" do
     expect(page).to have_content("Tech")
     expect(page).to have_content("Education")
   end
+  scenario "and sees total count of jobs for tag" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Teacher", level_of_interest: 70, city: "Denver")
+    tag1 = Tag.create(name: "Tech")
+    tag2 = Tag.create(name: "Education")
+    job.tags << tag1
+    job.tags << tag2
+    job1.tags << tag2
+
+    visit company_job_path(company, job)
+    save_and_open_page
+
+    expect(page).to have_content("1")
+    expect(page).to have_content("2")
+  end
 end
