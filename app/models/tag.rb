ccd989cb0f395935cfc227f6767c11c683e4f7e7
Mike Heft <mikeheft@gmail.com>
2017-09-19 09:40:12 -0600

    work on salary average for tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   has_many :job_tags
   has_many :jobs, through: :job_tags
   
+  def salary_average
+    require "pry"; binding.pry
+    jobs.average(:salary)
+  end
 end
--- spec/features/jobs/user_can_see_assoc_tags_spec.rb
+++ spec/features/jobs/user_can_see_assoc_tags_spec.rb
@@ -18,13 +18,30 @@ feature "User" do
       job = Job.create!(title: 'Thing', description: 'Does the thing', level_of_interest: 100, company_id: 1,
                       city: 'Denver', salary: 20000)
       job_2 = Job.create!(title: 'Thing', description: 'Does the thing', level_of_interest: 100, company_id: 2,
-                        city: 'Denver', salary: 20000)
+                        city: 'Denver', salary: 15000)
       job_2.tags << Tag.create!(name: 'DWS')
       job.tags << Tag.create!(name: 'DWS')
       visit company_job_path(company, job)
       
       expect(page).to have_content(job.tags.first.name)     
       expect(page).to have_content(job.tags.first.jobs.count)     
+    end
+    
+    scenario "see average salary jobs with that tag." do
+      company = Company.create!(id: 1, name: 'ESPN')
+      company = Company.create!(id: 2, name: 'Turing')
+      job = Job.create!(title: 'Thing', description: 'Does the thing', level_of_interest: 100, company_id: 1,
+                      city: 'Denver', salary: 20000)
+      job_2 = Job.create!(title: 'Thing', description: 'Does the thing', level_of_interest: 100, company_id: 2,
+                        city: 'Denver', salary: 15000)
+      tag = Tag.create!(name: 'DWS')
+      tag.jobs = [job, job_2]
+      require "pry"; binding.pry
+      job_2.tags << tag
+      job.tags << tag
+      visit company_job_path(company, job)
+      
+      expect(page).to have_content(job.tags.first.salary_average)     
     end
   end
 end
