341f3cd70a48ebe57a114c9af27d3888dd23bb5d
JunePaloma <kborkoski@gmail.com>
2017-09-19 10:49:25 -0600

    Add instance methods tests to tag spec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -17,4 +17,23 @@ describe Tag do
       expect(tag).to respond_to(:job_tags)
     end
   end
+
+  describe 'Instance Methods' do
+    it '#count' do
+      company = Company.create!(name: "ESPN")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 70000)
+      tag = Tag.create!(title: "Software")
+      job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
+
+      expect(tag.count).to eq(1)
+    end
+    it '#average_salary' do
+      company = Company.create!(name: "ESPN")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 70000)
+      tag = Tag.create!(title: "Software")
+      job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
+
+      expect(tag.average_salary).to eq(70000)
+    end
+  end
 end
