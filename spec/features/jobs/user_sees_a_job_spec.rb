69d80fbe660d22fa978951d57853f54bf235a754
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-19 09:20:02 -0600

    second test created

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -20,7 +20,30 @@ describe "User sees a specific job" do
     job.tags << tag
 
     visit company_job_path(company, job)
-save_and_open_page
+
     expect(page).to have_content(tag.name)
+  end
+
+  scenario "I also see a count of how many jobs have each specific tag listed." do
+    company = Company.create!(name: "ESPN")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Front-end Developer", level_of_interest: 70, city: "Denver")
+    job3 = company.jobs.create!(title: "Senior Developer", level_of_interest: 70, city: "Denver")
+    job4 = company.jobs.create!(title: "Site Reliability Eng", level_of_interest: 70, city: "Denver")
+    job5 = company.jobs.create!(title: "Product Manager", level_of_interest: 70, city: "Denver")
+
+    tag = Tag.create(name: "Software")
+
+    job1.tags << tag
+    job2.tags << tag
+    job3.tags << tag
+    job4.tags << tag
+    job5.tags << tag
+
+
+    visit company_job_path(company, job1)
+
+    expect(page).to have_content(tag.name)
+    expect(page).to have_content("Software - 5 ")
   end
 end
