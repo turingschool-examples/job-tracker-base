dee650661970a2fbf6afbe832a749e250ea8f5a0
snayrouz <samuel.nayrouz@gmail.com>
2017-09-19 09:19:02 -0600

    Final commit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,13 @@
 class Tag < ApplicationRecord
   has_many :job_tags
   has_many :jobs, through: :job_tags
+
+  def job_count
+    jobs.count
+  end
+
+  def avg_job_salary
+    jobs.average(:salary)
+  end
+  
 end
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -12,7 +12,7 @@ describe "User sees a specific job" do
     expect(page).to have_content("70")
   end
 
-  scenario 'a user sees the name of each tag for a specific job' do
+  scenario 'a user sees the name & count of each tag for a specific job' do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
     tag = job.tags.create(name: "Software")
@@ -20,5 +20,17 @@ describe "User sees a specific job" do
     visit company_job_path(company, job)
 
     expect(page).to have_content("Software")
+    expect(page).to have_content(1)
   end
+
+  scenario 'a user sees the average salary for a specified job' do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 22000)
+    tag = job.tags.create(name: "Software")
+    
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(22000)
+  end
+
 end
