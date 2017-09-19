8e89f769e63cbd23a3a9c3c7c15cdee14b282f66
JunePaloma <kborkoski@gmail.com>
2017-09-19 09:43:12 -0600

    Add model for job_tags, update test

--- spec/features/jobs/user_sees_job_show_page_spec.rb
+++ spec/features/jobs/user_sees_job_show_page_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 describe "As a user, I visit a specific job page," do
   it "I see the name of each of the tags associated with that job." do
     company = Company.create!(name: "ESPN")
-    job_tag = JobTag.create!(title: "Software")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", job_tag_id: job_tag.id)
+    tag = Tag.create!(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job_tag = JobTag.create!(job_id:job.id, tag_id: tag.id)
+
+
 
     visit company_job_path(company, job)
 
