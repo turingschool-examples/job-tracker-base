6145197db47abae516dd0c6305fefeaaf6d26e82
snayrouz <samuel.nayrouz@gmail.com>
2017-09-19 09:08:44 -0600

    Create migrations for tag and job tags

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,14 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+  scenario 'a user sees the name of each tag for a specific job' do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag = job.tags.create(name: "Software")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Software")
+  end
 end
