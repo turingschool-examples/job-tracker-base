672bc83f9912cf7da3cfcc6b76d31d26e44a7c06
Sam Lim <limsammy1@gmail.com>
2017-09-19 09:29:34 -0600

    Set up feature test for story 1

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,17 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+  scenario "a user sees the tags for a job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag_1 = job.tags.create(title: 'Software')
+    tag_2 = job.tags.create(title: 'Fun')
+    # As a user,
+    # When I visit a specific job page,
+    visit company_job_path(company, job)
+    # I see the name of each of the tags associated with that job.
+    expect(page).to have_content(tag_1.title)
+    expect(page).to have_content(tag_2.title)
+  end
 end
