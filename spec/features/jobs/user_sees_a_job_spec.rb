5e3e657c46bd828da71f55f057e7e09f5c1fecfc
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-19 09:07:14 -0600

    set up first test for tags

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,16 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+  scenario "a user sees the mame of each of the tags associated with that job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag = Tag.create(name: "Software")
+
+    job.tags << tag
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(tag.name)
+  end
 end
