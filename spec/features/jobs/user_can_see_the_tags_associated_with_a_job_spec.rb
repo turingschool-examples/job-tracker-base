dc8cd073f120b65cabdf759b65393ddb89eb33a9
Brandon Stewart <boveus@gmail.com>
2017-09-19 09:15:24 -0600

    Add jobs can have tags.

--- spec/features/jobs/user_can_see_the_tags_associated_with_a_job_spec.rb
+++ spec/features/jobs/user_can_see_the_tags_associated_with_a_job_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 describe "User visits a job page" do
   scenario "a user can see tags associated with a job" do
+    company = Company.create!(name: "ESPN")
 
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    tag = job.tags.create(text: "Cool location")
+    tag2 = job.tags.create(text: "Good benefits")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(tag.text)
+    expect(page).to have_content(tag2.text)
   end
 end
