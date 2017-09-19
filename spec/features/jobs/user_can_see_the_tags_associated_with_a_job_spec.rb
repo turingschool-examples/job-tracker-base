edec13933a0866153973dbdb9471d166136ee8d0
Brandon Stewart <boveus@gmail.com>
2017-09-19 09:44:26 -0600

    Add counts to show page + feature test for that thing.  User story 2+

--- spec/features/jobs/user_can_see_the_tags_associated_with_a_job_spec.rb
+++ spec/features/jobs/user_can_see_the_tags_associated_with_a_job_spec.rb
@@ -14,4 +14,18 @@ describe "User visits a job page" do
     expect(page).to have_content(tag.text)
     expect(page).to have_content(tag2.text)
   end
+  scenario "a user can see the count of jobs with a particular tag" do
+    company = Company.create!(name: "ESPN")
+
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Database Janitor", level_of_interest: 70, city: "Denver")
+
+    tag = job.tags.create(text: "Cool location")
+    tag2 = job2.tags.create(text: "Cool location")
+    tag3 = job.tags.create(text: "Good benefits")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("#{tag.text}(2)")
+  end
 end
