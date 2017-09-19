9fcaaa36ef698e14f918b99accd781e12794fc24
amhursh <amhursh@gmail.com>
2017-09-19 10:26:07 -0600

    Add has_many relationship test

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -33,4 +33,11 @@ describe Job do
       expect(job).to respond_to(:company)
     end
   end
+
+  describe "relationships" do
+    it "has_many tags" do
+      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+      expect(job).to respond_to(:tags)
+    end
+  end
 end
