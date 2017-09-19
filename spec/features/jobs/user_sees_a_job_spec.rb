eabd88971e9e95e4df47312e37deb498c5278a63
sidewinder2020 <sdwinder1@gmail.com>
2017-09-19 09:20:49 -0600

    create many to many relationship

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,20 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+  scenario "a user sees tags for that specific job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag_1 = job.tags.create!(title: "Good Location")
+    tag_2 = job.tags.create!(title: "Great Salary")
+    tag_3 = job.tags.create!(title: "Good Management")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("ESPN")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("70")
+    expect(page).to have_content("Good Management")
+    expect(page).to have_content("Great Salary")
+    expect(page).to have_content("Good Location")
+  end
 end
