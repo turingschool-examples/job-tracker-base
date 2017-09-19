e8cf05b245a1f5e087e342fbd9f0db52d5d6b185
amhursh <amhursh@gmail.com>
2017-09-19 09:41:04 -0600

    Add feature test for job count by tag on job page

--- spec/features/tags/user_can_see_all_tags_for_a_job_spec.rb
+++ spec/features/tags/user_can_see_all_tags_for_a_job_spec.rb
@@ -20,5 +20,53 @@ describe "User can see all tags for a job" do
 			expect(page).to have_content("Classic Sal")
 			expect(page).to have_content("Code")
 		end
+
+		scenario "and can see the number of jobs associated with each tag" do
+			company = Company.create(name: "Turing")
+			job1 = Job.create(title: "Teacher",
+											 description: "Teaches things",
+											 level_of_interest: 49,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 1000000)
+			job2 = Job.create(title: "Pizza Guy",
+											 description: "Delivers Pizza",
+											 level_of_interest: 5,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 55555)
+			job3 = Job.create(title: "Student",
+											 description: "Learns things",
+											 level_of_interest: 49,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 1)
+			job4 = Job.create(title: "Accountant",
+											 description: "Counts things",
+											 level_of_interest: 3,
+											 company_id: company.id,
+											 city: "Denver",
+											 salary: 99999)
+			tag1 = Tag.create(name: "Cool")
+			tag2 = Tag.create(name: "Classic Sal")
+			tag3 = Tag.create(name: "Code")
+
+			JobsTag.create(job_id: job1.id, tag_id: tag1.id)
+			JobsTag.create(job_id: job1.id, tag_id: tag2.id)
+			JobsTag.create(job_id: job1.id, tag_id: tag3.id)
+			JobsTag.create(job_id: job2.id, tag_id: tag1.id)
+			JobsTag.create(job_id: job2.id, tag_id: tag2.id)
+			JobsTag.create(job_id: job3.id, tag_id: tag1.id)
+
+			visit company_job_path(company, job1)
+
+			expect(page).to have_content("3 other job(s) with this tag")
+			expect(page).to have_content("2 other job(s) with this tag")
+			expect(page).to have_content("1 other job(s) with this tag")
+		end
 	end
 end
+
+# As a user,
+# When I visit a specific job page,
+# I also see a count of how many jobs have each specific tag listed.
