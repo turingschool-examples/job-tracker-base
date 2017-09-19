7c635dfadcc529506a1e7bfa79f7bb448dd7226e
JunePaloma <kborkoski@gmail.com>
2017-09-19 09:50:42 -0600

    Add test for second user story

--- spec/features/jobs/user_sees_job_show_page_spec.rb
+++ spec/features/jobs/user_sees_job_show_page_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 describe "As a user, I visit a specific job page," do
-  it "I see the name of each of the tags associated with that job." do
-    company = Company.create!(name: "ESPN")
-    tag = Tag.create!(title: "Software")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
 
-    visit company_job_path(company, job)
+  company = Company.create!(name: "ESPN")
+  tag = Tag.create!(title: "Software")
+  job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+  job2 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Chicago")
+  job_tag = JobTag.create!(job_id:job.id, tag_id:tag.id)
+  job_tag2 = JobTag.create!(job_id:job2.id, tag_id:tag.id)
 
-    expect(page).to have_content("Software")
+    it "I see the name of each of the tags associated with that job." do
+      visit company_job_path(company, job)
+      expect(page).to have_content("Software")
+    end
 
+    it "I also see a count of how many jobs have each specific tag listed." do
+      visit company_job_path(company, job)
+      expect(page).to have_content("Software jobs: 2")
   end
+
 end
