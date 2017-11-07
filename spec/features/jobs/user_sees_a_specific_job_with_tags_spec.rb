79cdec1516a42ae4b949323fc6e952acb4382f20
Dione Wilson <dionew1@gmail.com>
2017-11-07 09:39:33 -0700

    Tag Association Made with Job via Joins table:

---
+++ spec/features/jobs/user_sees_a_specific_job_with_tags_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "User sees a specific job" do
+  scenario "with tags associated to that job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job.tags.create(name: "Tech")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(job.tags.last.name)
+  end
+
+  scenario "with tags associated to that job" do
+    company = Company.create!(name: "ESPN")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag1 = Tag.create!(name: "Tech")
+    JobTag.create(job: job1, tag: tag1)
+    job2 = company.jobs.create!(title: "Software Engineer", level_of_interest: 80, city: "Dallas")
+    JobTag.create(job: job2, tag: tag1)
+  
+    visit company_job_path(company, job1)
+
+    expect(page).to have_content(2)
+  end
+end
