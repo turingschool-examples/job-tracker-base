5ec924c030ed626adb5e3a3e69c439c8568644e8
Marshall Houston <marshall.houston@gmail.com>
2017-11-07 09:41:00 -0700

    move setup to before method

--- spec/features/jobs/user_sees_all_tags_for_a_job_spec.rb
+++ spec/features/jobs/user_sees_all_tags_for_a_job_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe "When a visit a specific job page" do
   describe "as a user" do
+    before :each do
+      @company = Company.create!(name: "ESPN")
+      @job = @company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+      @job.tags.create(name: "Great-company")
+      @job.tags.create(name: "Software")
+
+      visit company_job_path(@company, @job)
+    end
     scenario "I see all tags associated with a job" do
-      company = Company.create!(name: "ESPN")
-      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-
-      job.tags.create(name: "Great-company")
-      job.tags.create(name: "Software")
-
-      visit company_job_path(company, job)
-
       expect(page).to have_content("ESPN")
       expect(page).to have_content("Developer")
       expect(page).to have_content("70")
@@ -19,6 +20,9 @@ describe "When a visit a specific job page" do
         expect(page).to have_content("Great-company - 1")
         expect(page).to have_content("Software - 1")
       end
+    end
+
+    xscenario "I see tags that have more than just one job" do
 
     end
   end
