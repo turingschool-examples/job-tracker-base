c33a2a0a16a3f18b03e0ecba9d847c18c8ae9753
Emily Wise <wise.m.emily@gmail.com>
2017-11-07 10:15:05 -0700

    added test for final story

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,6 @@ class Tag < ApplicationRecord
   has_many :jobs, through: :job_tags
 
   def job_count
-
     jobs.count
   end
 end
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -48,13 +48,38 @@ describe "User sees a specific job" do
     job_6.job_tags.create!(tag: tag_2)
 
 
-
     visit company_job_path(company, job_1)
-
 
 
     expect(page).to have_content("Software - 4")
     expect(page).to have_content("Good-Location - 3")
   end
+
+  scenario "I also see an average salary for all jobs within each specific tag listed" do
+    company = Company.create!(name: "ESPN")
+    job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 50000)
+    job_2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", salary: 60000)
+    job_3= company.jobs.create!(title: "Marketing", level_of_interest: 80, city: "Boston", salary: 60000)
+    job_4 = company.jobs.create!(title: "Clown", level_of_interest: 70, city: "New York City", salary: 80000)
+    job_5 =company.jobs.create!(title: "Dog-walker", level_of_interest: 70, city: "New York City", salary: 90000)
+    job_6 = company.jobs.create!(title: "Trainer", level_of_interest: 70, city: "New York City", salary: 10000)
+    tag_1 = Tag.create!(title: "Software")
+    tag_2 = Tag.create!(title: "Good-Location")
+
+    job_1.job_tags.create!(tag: tag_1)
+    job_1.job_tags.create!(tag: tag_2)
+    job_2.job_tags.create!(tag: tag_1)
+    job_3.job_tags.create!(tag: tag_1)
+    job_4.job_tags.create!(tag: tag_2)
+    job_4.job_tags.create!(tag: tag_1)
+    job_6.job_tags.create!(tag: tag_2)
+
+    visit company_job_path(company, job_1)
+
+
+    expect(page).to have_content("Software - 4 (62500)")
+    expect(page).to have_content("Good-Location - 3 (46666)")
+  end
+
 
 end
