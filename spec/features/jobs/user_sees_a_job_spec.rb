b850a5e5c2f78e5891fa305ded4906b93a20255b
BLaurenB <blbillington1@gmail.com>
2017-11-07 10:53:22 -0700

    had a mistake in my tests the whole time

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -30,26 +30,31 @@ describe "User sees a specific job" do
   scenario "user sees how many jobs have each specific tag listed" do
     #does this mean on 1 show page, I list all the other jobs that share the same tags?
     company = Company.create(name: "ESPN")
+
+    tag_1 = Tag.create(title:"FE")
+    tag_2 = Tag.create(title:"BE")
+    tag_3 = Tag.create(title:"Full stack")
+
     job_1 = company.jobs.create(title: "Developer", level_of_interest: 70, city: "Denver")
-    job_1.tags.create(title: "FE")
-    job_1.tags.create(title: "BE")
-    job_1.tags.create(title: "Full Stack")
+    job_1.tags << tag_1
+    job_1.tags << tag_2
+    job_1.tags << tag_3
 
     job_2 = company.jobs.create(title: "Also Developer", level_of_interest: 50, city: "Denver")
-    job_2.tags.create(title: "FE")
-    job_2.tags.create(title: "BE")
-    job_2.tags.create(title: "Full Stack")
+    job_2.tags << tag_1
+    job_2.tags << tag_2
+
 
     job_3 = company.jobs.create(title: "Also Also Developer", level_of_interest: 20, city: "Denver")
-    job_3.tags.create(title: "Full Stack")
+    job_3.tags << tag_1
 
     visit company_job_path(company, job_1)
     save_and_open_page
 
 
-    expect(page).to have_content("Other Jobs Tagged with FE: 2")
+    expect(page).to have_content("Other Jobs Tagged with FE: 3")
     expect(page).to have_content("Other Jobs Tagged with BE: 2")
-    expect(page).to have_content("Other Jobs Tagged with Full Stack: 3")
+    expect(page).to have_content("Other Jobs Tagged with Full Stack: 1")
   end
 
 end
