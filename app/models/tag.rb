f3da31c56cc9da5314d9bc429c9e30d5d6495048
Josi <junipertmcclellan@gmail.com>
2017-11-07 10:08:39 -0700

    add tag helpers to support job show view

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,13 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
   has_and_belongs_to_many :jobs
+
+  def job_count
+    jobs.count
+  end
+
+  def mean_salary
+    jobs.average(:salary).to_i
+  end
+
 end
