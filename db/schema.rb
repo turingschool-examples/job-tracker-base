7625788f878400a369e4f1f7256db09898a355b3
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-07 09:26:22 -0700

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731144005) do
+ActiveRecord::Schema.define(version: 20171107161813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20170731144005) do
     t.string   "city"
     t.integer  "salary"
     t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
   end
 
   add_foreign_key "jobs", "companies"
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,17 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+scenario "a user sees a tags for a job for a specific company" do
+  company = Company.create!(name: "ESPN")
+  job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+  tag_1 = Tag.create(name: "Great Location")
+  tag_2 = Tag.create(name: "Great Benefits")
+  
+  visit company_job_path(company, job)
+
+  expect(page).to have_content("ESPN")
+  expect(page).to have_content("Developer")
+  expect(page).to have_content("70")
+
 end
