3977bb97b2dbae0edbebb79830c8e5fa08a4c0df
randy-darsh <rspringer47@gmail.com>
2017-11-07 10:42:27 -0700

    all stories passing

--- spec/user/user_visits_a_job_page_spec.rb
+++ spec/user/user_visits_a_job_page_spec.rb
@@ -4,30 +4,25 @@ feature 'User visits a jobs show page' do
   before :each do
     company = Company.create(name: "Company")
     job = Job.create(title: "Job", description: "You do things", level_of_interest: 1, company: company, city: "Place", salary: 12)
-    job2 = Job.create(title: "Job2", description: "You do different things", level_of_interest: 1, company: company, city: "Place", salary: 70)
-    job.tags.create(name: "Cool")
-    job.tags.create(name: "Not Cool")
-    job.tags.create(name: "Hella Cool")
-    job2.tags.create(name: "Cool")
-    job2.tags.create(name: "Not Cool")
-    job2.tags.create(name: "Hella Cool")
+    @tag1 = job.tags.create(name: "Cool")
+    @tag2 = job.tags.create(name: "Not Cool")
+    @tag3 = job.tags.create(name: "Hella Cool")
+    job2 = Job.create(title: "Job2", description: "You do different things", level_of_interest: 1, company: company, city: "Place", salary: 70, tags: [@tag1, @tag2, @tag3])
 
     visit "/companies/#{company.id}/jobs/#{job.id}"
   end
 
   it 'they see the name of the tags associated with that job' do
-    expect(page).to have_content("Cool")
-    expect(page).to have_content("Not Cool")
-    expect(page).to have_content("Hella Cool")
+    expect(page).to have_content("Tag: Cool")
+    expect(page).to have_content("Tag: Not Cool")
+    expect(page).to have_content("Tag: Hella Cool")
   end
 
   it 'they see a count of all jobs related to that tag' do
-    expect(page).to have_content("Number of jobs with this tag: 2")
+    expect(page).to have_content("Number of jobs with the tag: 2")
   end
 
   it 'they see the average salary for all jobs in a specific tag' do
+    expect(page). to have_content("Average Salary of job with this tag: 41")
   end
 end
-# As a user,
-# When I visit a specific job page,
-# I see the name of each of the tags associated with that job.
