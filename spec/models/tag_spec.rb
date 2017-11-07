e32f6efb96885a38738e19d751425e33a44f0019
Josi <junipertmcclellan@gmail.com>
2017-11-07 09:38:37 -0700

    add tag model spec

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Tag do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new
+        expect(tag).to be_invalid
+      end
+    end
+
+    context "invalid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: "red")
+        expect(tag).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      tag = Tag.new(name: "red")
+      expect(job).to respond_to(:jobs)
+    end
+  end
+end
