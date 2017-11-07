6da413c5bd2910782395004faec03b8b003af430
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-07 09:11:28 -0700

    outline job show feature tests

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,26 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+  scenario "a user sees tags for a specific job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_job_path(company, job)
+#     As a user,
+#     When I visit a specific job page,
+#     I see the name of each of the tags associated with that job.
+  end
+
+  scenario "a user sees count of jobs for each tag for a job" do
+  # As a user,
+  # When I visit a specific job page,
+  # I also see a count of how many jobs have each specific tag listed.
+  end
+
+  scenario "a user sees the average salary for all jobs with a tag" do
+    # As a user,
+    # When I visit a specific job page,
+    # I also see an average salary for all jobs within each specific tag listed.
+  end
 end
