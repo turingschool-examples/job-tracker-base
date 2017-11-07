7deea69bbe4b2494375c4771c073bd08574b0ee3
Marshall Houston <marshall.houston@gmail.com>
2017-11-07 09:51:26 -0700

    passing tests for tags with more than 1 job. doesnt feel robust though

--- spec/features/jobs/user_sees_all_tags_for_a_job_spec.rb
+++ spec/features/jobs/user_sees_all_tags_for_a_job_spec.rb
@@ -4,10 +4,10 @@ describe "When a visit a specific job page" do
   describe "as a user" do
     before :each do
       @company = Company.create!(name: "ESPN")
-      @job = @company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+      @job = @company.jobs.create!(title: "Developer", description: "sports", salary: 60000, level_of_interest: 70, city: "Denver")
 
-      @job.tags.create(name: "Great-company")
-      @job.tags.create(name: "Software")
+      @tag1 = @job.tags.create(name: "Great-company")
+      @tag2 = @job.tags.create(name: "Software")
 
     end
     scenario "I see all tags associated with a job" do
@@ -25,10 +25,10 @@ describe "When a visit a specific job page" do
 
     scenario "I see tags that have more than just one job" do
       company2 = Company.create!(name: "Guild Education")
-      job2 = company2.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+      job2 = company2.jobs.create!(title: "Developer", description: "education", salary: 70000, level_of_interest: 90, city: "Denver")
 
-      job2.tags.create(name: "Great-company")
-      job2.tags.create(name: "Software")
+      job2.tags << @tag1
+      job2.tags << @tag2
       job2.tags.create(name: "Education")
 
       visit company_job_path(company2, job2)
@@ -36,9 +36,8 @@ describe "When a visit a specific job page" do
       within ".job-tags" do
         expect(page).to have_content("Great-company - 2")
         expect(page).to have_content("Software - 2")
-        expect(page).to have_content("Software - 1")
+        expect(page).to have_content("Education - 1")
       end
-
     end
   end
 end
