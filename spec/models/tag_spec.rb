ee50f3e62ed82daf7ff5849016aecd0ad2b7f0c5
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-07 10:04:17 -0700

    make test for tag#jobs_with_tag pass

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -29,4 +29,22 @@ describe Tag do
       expect(tag).to respond_to(:jobs)
     end
   end
+
+  describe "instance methods" do
+    it '#jobs_with_tag finds the number of jobs with a specific tag' do
+      company = Company.create!(name: "ESPN")
+      job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+      job2 = company.jobs.create!(title: "Technician", level_of_interest: 50, city: "Longmont")
+      tag1 = job1.tags.create!(title: "Software")
+      tag2 = job1.tags.create!(title: "Far Away")
+      job2.tags << tag1
+
+      expect(tag1.jobs_with_tag).to eq(2)
+      expect(tag2.jobs_with_tag).to eq(1)
+    end
+
+    it '#average_salary calculates the average salary of all jobs with a specific tag' do
+
+    end
+  end
 end
