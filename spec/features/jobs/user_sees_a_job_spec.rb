6872df3cb892be91ba735dc426d4362b4149bea4
Anna Lewis <anna.noel.lewis@gmail.com>
2017-11-07 09:30:44 -0700

    flush out feature tests for job show

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -14,9 +14,17 @@ describe "User sees a specific job" do
 
   scenario "a user sees tags for a specific job" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Technician", level_of_interest: 50, city: "Longmont")
+    tag1 = job1.tags.create!(title: "Software")
+    job1.tags.create!(title: "Far Away")
+    job2.tags << tag1
 
-    visit company_job_path(company, job)
+    visit company_job_path(company, job1)
+
+    expect(page).to have_content("Software")
+    expect(page).to have_content("Far Away")
+
 #     As a user,
 #     When I visit a specific job page,
 #     I see the name of each of the tags associated with that job.
@@ -24,9 +32,17 @@ describe "User sees a specific job" do
 
   scenario "a user sees count of jobs for each tag for a job" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Technician", level_of_interest: 50, city: "Longmont")
+    tag1 = job1.tags.create!(title: "Software")
+    job1.tags.create!(title: "Far Away")
+    job2.tags << tag1
 
-    visit company_job_path(company, job)
+    visit company_job_path(company, job1)
+
+    expect(page).to have_content(2)
+    expect(page).to have_content(1)
+
   # As a user,
   # When I visit a specific job page,
   # I also see a count of how many jobs have each specific tag listed.
@@ -34,9 +50,17 @@ describe "User sees a specific job" do
 
   scenario "a user sees the average salary for all jobs with a tag" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", salary: 80000)
+    job2 = company.jobs.create!(title: "Technician", level_of_interest: 50, city: "Longmont", salary: 40000)
+    tag1 = job1.tags.create!(title: "Software")
+    job1.tags.create!(title: "Far Away")
+    job2.tags << tag1
 
     visit company_job_path(company, job)
+
+    expect(page).to have_content(80000)
+    expect(page).to have_content(60000)
+    
     # As a user,
     # When I visit a specific job page,
     # I also see an average salary for all jobs within each specific tag listed.
