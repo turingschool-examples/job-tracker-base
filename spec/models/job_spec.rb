a267fc15e80ca9c270301c4fbca7bec4c4cdb71c
Emily Wise <wise.m.emily@gmail.com>
2017-11-07 10:31:01 -0700

    Completed challenge

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -32,5 +32,10 @@ describe Job do
       job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
       expect(job).to respond_to(:company)
     end
+
+    it "has many tags" do
+      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+      expect(job).to respond_to(:tags)
+    end
   end
 end
