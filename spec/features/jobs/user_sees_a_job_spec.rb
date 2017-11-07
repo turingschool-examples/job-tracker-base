61d2eb4ef3ad9c6f8bd34b06876d72705c96db7a
Adrian Lara <adrianblara@gmail.com>
2017-11-07 09:14:42 -0700

    adjust user sees job page spec to reflect existence of tags

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 describe "User sees a specific job" do
+
+  before(:each) do
+    @company = Company.create!(name: "ESPN")
+    @job_1 = @company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    @tag_1 = @job_1.tags.create(name: "Sports")
+    @tag_2 = @job_1.tags.create(name: "Entry Level")
+
+    visit company_job_path(@company, @job_1)
+  end
+
   scenario "a user sees a job for a specific company" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-
-    visit company_job_path(company, job)
-
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
+    expect(page).to have_content("Sports")
+    expect(page).to have_content("Entry Level")
   end
 end
