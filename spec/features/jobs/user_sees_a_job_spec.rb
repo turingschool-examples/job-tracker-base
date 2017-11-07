54a58c555a58f0c0d9c1b6cc595cf49a75ab1df9
Emily Wise <wise.m.emily@gmail.com>
2017-11-07 09:46:19 -0700

    add failing test for user story 2

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -25,4 +25,31 @@ describe "User sees a specific job" do
     expect(page).to have_content("Good-Location")
 
   end
+
+  scenario "I also see a count of how many jobs have each specific tag listed." do
+    company = Company.create!(name: "ESPN")
+
+
+    job_1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job_2 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+    job_3= company.jobs.create!(title: "Marketing", level_of_interest: 80, city: "Boston")
+    job_4 = company.jobs.create!(title: "Clown", level_of_interest: 70, city: "New York City")
+    job_5 =company.jobs.create!(title: "Dog-walker", level_of_interest: 70, city: "New York City")
+    job_6 = company.jobs.create!(title: "Trainer", level_of_interest: 70, city: "New York City")
+
+    job_1.tags.create!(title: "Software")
+    job_1.tags.create!(title: "Good-Location")
+    job_2.tags.create!(title: "Software")
+    job_3.tags.create!(title: "Software")
+    job_4.tags.create!(title: "Good-Location")
+    job_5.tags.create!(title: "Good-Location")
+    job_6.tags.create!(title: "Software")
+
+    visit company_job_path(company, job_1)
+
+
+    expect(page).to have_content("Software - 4")
+    expect(page).to have_content("Good-Location - 3")
+  end
+
 end
