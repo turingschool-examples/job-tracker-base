3e3aa5fff7ae7629a57d672a08835af9040f4dae
Emily Wise <wise.m.emily@gmail.com>
2017-11-07 09:21:13 -0700

    add user story test 1

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -11,4 +11,16 @@ describe "User sees a specific job" do
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
   end
+
+  scenario "I see the name of each of the tags associated with that job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag_1 = job.tags.create!(titel: "Software")
+    tag_2 = job.tags.create!(titel: "Good-Location")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("Software")
+    expect(page).to have_content("Good-Location")
+  end
 end
