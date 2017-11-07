79afb2e12e506eae4ffa3de0cdcaad2a613b24ce
Emily Wise <wise.m.emily@gmail.com>
2017-11-07 10:27:45 -0700

    completed tag relationship model test

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it "has many to a jobs" do
+      tag = Tag.new(title: "Software")
+      expect(tag).to respond_to(:jobs)
+    end
+
+
+      it "has many to a job_tags" do
+        tag = Tag.new(title: "Software")
+        expect(tag).to respond_to(:job_tags)
+      end
+  end
 end
