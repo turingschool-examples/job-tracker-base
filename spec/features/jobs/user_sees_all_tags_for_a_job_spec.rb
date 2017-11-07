9b9fb7c8e4440f99218ffc783efb2451521afe10
Marshall Houston <marshall.houston@gmail.com>
2017-11-07 09:16:19 -0700

    create test for job tags

---
+++ spec/features/jobs/user_sees_all_tags_for_a_job_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "When a visit a specific job page" do
+  describe "as a user" do
+    scenario "I see all tags associated with a job" do
+      company = Company.create!(name: "ESPN")
+      job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+      job.tags.create(name: "Great-company")
+      job.tags.create(name: "Software")
+
+      visit company_job_path(company, job)
+
+      expect(page).to have_content("ESPN")
+      expect(page).to have_content("Developer")
+      expect(page).to have_content("70")
+
+      within ".job-tags" do
+        expect(page).to have_content("Great-company")
+        expect(page).to have_content("Software")
+      end
+
+    end
+  end
+end
