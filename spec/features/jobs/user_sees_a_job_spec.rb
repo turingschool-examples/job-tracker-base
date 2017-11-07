c602726c1ba6ecbd26083c44c7de7dc514671058
Adrian Lara <adrianblara@gmail.com>
2017-11-07 09:33:15 -0700

    job count of each tag is visible from job show page

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -8,6 +8,13 @@ describe "User sees a specific job" do
     @tag_1 = @job_1.tags.create(name: "Sports")
     @tag_2 = @job_1.tags.create(name: "Entry Level")
 
+    @job_2 = @tag_1.jobs.create(title: "Product Manager", level_of_interest: 10, city: "Denver", company: @company)
+    @job_3 = @tag_1.jobs.create(title: "Code Monkey", level_of_interest: 99, city: "Denver", company: @company)
+
+    @job_4 = @tag_2.jobs.create(title: "Code Monkey", level_of_interest: 95, city: "Seattle", company: @company)
+    @job_5 = @tag_2.jobs.create(title: "Junior Developer", level_of_interest: 90, city: "Seattle", company: @company)
+    @job_6 = @tag_2.jobs.create(title: "Director", level_of_interest: 85, city: "Seattle", company: @company)
+
     visit company_job_path(@company, @job_1)
   end
 
@@ -15,7 +22,11 @@ describe "User sees a specific job" do
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
-    expect(page).to have_content("Sports")
-    expect(page).to have_content("Entry Level")
   end
+
+  scenario "a user sees a job's tags and number of jobs associated with that tag" do
+    expect(page).to have_content("Sports - 3")
+    expect(page).to have_content("Entry Level - 4")
+  end
+
 end
