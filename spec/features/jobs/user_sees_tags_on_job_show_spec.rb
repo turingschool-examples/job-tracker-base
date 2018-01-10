25b2acbcf3204a4dbe9e22104e73d05df3f90a90
katyjane8 <katy.jane8@gmail.com>
2018-01-10 13:25:31 -0700

    story 1 done

--- spec/features/jobs/user_sees_tags_on_job_show_spec.rb
+++ spec/features/jobs/user_sees_tags_on_job_show_spec.rb
@@ -4,8 +4,10 @@ describe "User sees a specific job with associated tags" do
   scenario "a user sees a tags on the jobs" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    tag1 = Tag.create!(name: "sportsball", job_id: 1)
-    tag2 = Tag.create!(name: "throwing things", job_id: 1)
+    tag1 = Tag.create!(name: "sportsball")
+    tag2 = Tag.create!(name: "throwing things")
+    JobTag.create!(job: job, tag: tag1)
+    JobTag.create!(job: job, tag: tag2)
 
     visit company_job_path(company, job)
 
