800c2a711f4aac8cd0dd0b9cda9a3730f3e9a1b9
katyjane8 <katy.jane8@gmail.com>
2018-01-10 13:35:26 -0700

    through user story 2

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
-  has_many :job_tags 
+  has_many :job_tags
   has_many :jobs, through: :job_tags
+
+  def count_jobs
+    jobs.count
+  end
 end
--- spec/features/jobs/user_sees_tags_on_job_show_spec.rb
+++ spec/features/jobs/user_sees_tags_on_job_show_spec.rb
@@ -15,4 +15,22 @@ describe "User sees a specific job with associated tags" do
     expect(page).to have_content("throwing things")
     expect(page).to have_content("Developer")
   end
+
+  scenario "a user sees a tags on the jobs" do
+    company = Company.create!(name: "ESPN")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "HR", level_of_interest: 50, city: "SF")
+    job3 = company.jobs.create!(title: "CTO", level_of_interest: 50, city: "SF")
+    tag1 = Tag.create!(name: "sportsball")
+    JobTag.create!(job: job1, tag: tag1)
+    JobTag.create!(job: job2, tag: tag1)
+    JobTag.create!(job: job3, tag: tag1)
+
+    visit company_job_path(company, job1)
+
+    expect(page).not_to have_content("2")
+    expect(page).to have_content("3")
+    expect(page).to have_content("sportsball")
+  end
+
 end
